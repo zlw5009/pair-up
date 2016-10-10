600fd5c5e492127be465cf28ec9924a93ec3be4b
Zachary Williams <zlw5009@gmail.com>
2016-10-10 06:21:36 -0400

    Added user registration to DB

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -4,17 +4,25 @@ before do
 end
 
 helpers do
-  def passwords_match?(password, confirm)
-    return password == confirm
-  end
-
   def validate_password?(password, confirm)
     # pw criteria - 1 alpha char, 1 digit, 8-24
-    if (passwords_match?(password, confirm))
+    if (password == confirm)
       return password.match(/^(?=.*[a-z])(?=.*\d).{8,24}$/)
     else
       return false
     end
+  end
+
+  def validate_user?(email, password)
+
+  end
+
+  def passwords_match?(password)
+
+  end
+
+  def current_user(email)
+    @storage.user_info(email)
   end
 end
 
@@ -32,14 +40,40 @@ post '/users/signup' do
 
   if (validate_password?(password, confirm))
     @storage.new_user(name, email, password)
-    redirect '/users/register'
+    user_info = current_user(email)
+
+    redirect "/users/#{user_info[0][:id]}/register"
   else
     redirect '/users/signup'
   end
 end
 
-get '/users/register' do
+# User registration form
+get '/users/:id/register' do
+  @user_id = params[:id].to_i
+
   erb :"users/register", layout: :"layouts/layout"
+end
+
+# Update user skills
+post '/users/:id/register' do
+  user_id = params[:id].to_i
+  ruby = params[:ruby].to_i
+  sinatra = params[:sinatra].to_i
+  rails = params[:rails].to_i
+  sql = params[:sql].to_i
+  javascript = params[:javascript].to_i
+  jquery = params[:jquery].to_i
+  html = params[:html].to_i
+  css = params[:css].to_i
+  github = params[:github].to_i
+  heroku = params[:heroku].to_i
+
+  binding.pry
+
+  @storage.update_user_skills(user_id, ruby, sinatra, rails, sql, javascript, jquery, html, css, github, heroku)
+
+  redirect "users/#{user_id}/dashboard"
 end
 
 get '/users/:id/dashboard' do
--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,6 @@
 # Database and business logic
 require 'pg'
+require 'pry'
 
 class Users
   def initialize
@@ -14,5 +15,35 @@ class Users
     sql = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3);"
 
     query(sql, name, email, password)
+  end
+
+  def user_info(email)
+    sql = "SELECT * FROM users WHERE email = $1"
+
+    result = query(sql, email)
+
+    result.map { |tuple| user_info_tuple_to_hash(tuple) }
+  end
+
+  def update_user_skills(user_id, ruby, sinatra, rails, sql, javascript, jquery, html, css, github, heroku)
+    sql = <<~SQL
+      INSERT INTO skillset (ruby, sinatra, rails, sql, javascript,
+        jquery, html, css, github, heroku, user_id) 
+      VALUES (#{ruby}, #{sinatra}, #{rails}, #{sql}, #{javascript},
+        #{jquery}, #{html}, #{css}, #{github}, #{heroku}, #{user_id});
+    SQL
+
+    query(sql)
+  end
+
+  private
+
+  def user_info_tuple_to_hash(tuple)
+    {id: tuple["id"].to_i,
+     name: tuple["name"],
+     email: tuple["email"],
+     password: tuple["password"],
+     current_team_id: tuple["current_team_id"].to_i,
+     current_challenge_id: tuple["current_challenge_id"].to_i}  
   end
 end
