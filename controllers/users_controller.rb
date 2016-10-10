67a9b816fbbf40834ce83282c32b1ab7807d19ea
Zachary Williams <zlw5009@gmail.com>
2016-10-10 01:13:44 -0400

    User sign in interaction with DB

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,6 +1,34 @@
 # Routes and helpers related to users
+before do
+  @storage = Users.new
+end
+
+helpers do
+  def passwords_match?(password, confirm)
+    return password == confirm
+  end
+end
 
 # New user form
+get '/users/signup' do
+  erb :"users/signup", layout: :"layouts/layout"
+end
+
+# create new user
+post '/users/signup' do
+  name = params[:name]
+  email = params[:email]
+  password = params[:password]
+  confirm = params[:confirm_password]
+
+  if (passwords_match?(password, confirm))
+    @storage.new_user(name, email, password)
+    redirect '/users/register'
+  else
+    redirect '/users/signup'
+  end
+end
+
 get '/users/register' do
   erb :"users/register", layout: :"layouts/layout"
 end
@@ -9,16 +37,4 @@ get '/users/:id/dashboard' do
   erb :"users/dashboard", layout: :"layouts/layout"
 end
 
-get '/users/1/dashboard' do
-  erb :"users/dashboard", layout: :"layouts/layout"
-end
-
-get '/users/signup' do
-  erb :"users/signup", layout: :"layouts/layout"
-end
-
-post '/users/signup' do
-  # this for a new user sign up? 
-  redirect '/users/register'
-end
 
--- models/user.rb
+++ models/user.rb
@@ -1,1 +1,18 @@
 # Database and business logic
+require 'pg'
+
+class Users
+  def initialize
+    @db = PG.connect(dbname: "pairup")
+  end
+
+  def query(statement, *params)
+    @db.exec_params(statement, params)
+  end
+
+  def new_user(name, email, password)
+    sql = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3);"
+
+    query(sql, name, email, password)
+  end
+end
